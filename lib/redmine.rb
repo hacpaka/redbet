7bc071627439858120002cbaa55ed5f913b9442c
hacpaka <hacpaka@eggbe.com>
2019-11-14 21:32:14 +0300

    +

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -171,6 +171,8 @@ Redmine::AccessControl.map do |map|
 end
 
 Redmine::MenuManager.map :top_menu do |menu|
+
+	menu.push :activity, {:controller => 'activities', :action => 'index'}, :caption => :label_activity
 	menu.push :my_page, {:controller => 'my', :action => 'page'}, :if => Proc.new { User.current.logged? }
 	menu.push :projects, {:controller => 'projects', :action => 'index'}, :caption => :label_project_plural
 	menu.push :administration, {:controller => 'admin', :action => 'index'}, :if => Proc.new { User.current.admin? }, :last => true
