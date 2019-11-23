c744bd1ab90045488d7a9428839b4205f23ec544
hacpaka <hacpaka@eggbe.com>
2019-11-23 06:45:23 +0300

    +

---
+++ db/migrate/20120601054557_create_custom_workflows_projects.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+#
+# Redmine plugin for Custom Workflows
+#
+# Copyright Anton Argirov
+# Copyright Karel Pičman <karel.picman@kontron.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class CreateCustomWorkflowsProjects < ActiveRecord::Migration[4.2]
+
+	def change
+		create_table :custom_workflows_projects, :force => true, :id => false do |t|
+			t.references :project
+			t.references :custom_workflow
+		end
+		add_index :custom_workflows_projects, [:project_id]
+		add_index :custom_workflows_projects, [:custom_workflow_id]
+	end
+
+end
---
+++ db/migrate/20150526132244_create_example_workflow.rb
@@ -0,0 +1,60 @@
+# encoding: utf-8
+#
+# Redmine plugin for Custom Workflows
+#
+# Copyright Anton Argirov
+# Copyright Karel Pičman <karel.picman@kontron.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class CreateExampleWorkflow < ActiveRecord::Migration[4.2]
+
+	def up
+		CustomWorkflow.reset_column_information
+		old = CustomWorkflow.where(:name => 'Duration/Done Ratio/Status correlation').first
+		old.destroy if old
+
+		CustomWorkflow.create!(:name => 'Duration/Done Ratio/Status correlation', :description => <<EOD, :before_save => <<EOS)
+Set up a correlation between the start date, due date, done ratio and status of issues.
+
+* If done ratio is changed to 100% and status is "In Process", status changes to "Resolved"
+* If status is "New", "Resolved" or "Feedback" and done ratio is changed to value less than 100%, status changes to "In process"
+* If status is changed to "In process" and start date is not set, then it sets to current date
+* If status is changed to "Resolved" and end date is not set, then it set to due date
+
+To use this script properly, turn off "Use current date as start date for new issues" option in the settings as this script already do it own way.
+EOD
+if @issue.done_ratio_changed?
+  if @issue.done_ratio==100 && @issue.status_id==2
+    @issue.status_id=3
+  elsif [1,3,4].include?(@issue.status_id) && @issue.done_ratio<100
+    @issue.status_id=2
+  end
+end
+
+if @issue.status_id_changed?
+  if @issue.status_id==2
+    @issue.start_date ||= Time.now
+  end
+  if @issue.status_id==3
+    @issue.done_ratio = 100
+    @issue.start_date ||= @issue.created_on
+    @issue.due_date ||= Time.now
+  end
+end
+EOS
+	end
+
+end
