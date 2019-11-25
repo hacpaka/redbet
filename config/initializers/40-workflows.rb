79e106bb53387a72f4563c5c1249ff56260ce8eb
hacpaka <hacpaka@eggbe.com>
2019-11-25 13:05:31 +0300

    +

---
+++ config/initializers/40-workflows.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+I18n.backend = Redmine::I18n::Backend.new
+# Forces I18n to load available locales from the backend
+I18n.config.available_locales = nil
+
+require 'redmine'
+require 'workflows'
